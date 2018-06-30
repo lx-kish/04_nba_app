7920c56ac9d81aa78b1742a92e838c9381ad4d70
lx kish <urban.ktulhu@gmail.com>
2018-06-30 18:59:40 +1200

    44 Layout and header before eject

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
+import Layout from './hoc/Layout/layout';
 
 class Routes extends Component {
-    render(){
+    render() {
         return (
-            <Switch>
-                <Route path="/" exact component={Home}/>
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </Layout>
+
         )
     }
 }
