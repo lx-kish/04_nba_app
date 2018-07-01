9f974101e77f975210ead1d7616d40b663166f73
lx kish <urban.ktulhu@gmail.com>
2018-07-01 21:42:33 +1200

    45 Adding Sidenav

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,18 +2,20 @@ import React from 'react';
 import style from './header.css';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
+import SideNav from './SideNav/sideNav';
 
-const Header = () => {
+const Header = (props) => {
 
     const navBars = () => {
         return (
             <div className={style.bars}>
                 <FontAwesome name="bars"
-                style={{
-                   color:'#dfdfdf',
-                   padding:'10px',
-                   cursor:'pointer' 
-                }}
+                    onClick={props.onOpenNav}
+                    style={{
+                        color: '#dfdfdf',
+                        padding: '10px',
+                        cursor: 'pointer'
+                    }}
                 />
             </div>
         )
@@ -27,6 +29,7 @@ const Header = () => {
 
     return (
         <header className={style.header}>
+            <SideNav {...props} />
             <div className={style.headerOpt}>
                 {navBars()}
                 {logo()}
