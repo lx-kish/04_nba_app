45e57201007dd6cfbc0aa501cada12c7d733849c
lx kish <urban.ktulhu@gmail.com>
2018-07-03 07:04:07 +1200

    51 News List cards

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import NewsSlider from '../widgets/NewsSlider/slider'
+import NewsList from '../widgets/NewsList/newsList';
 
 const Home = () => {
     return (
         <div>
             <NewsSlider
                 type="featured"
-                start={3}
-                end={6}
+                start={0}
+                end={3}
                 settings={{
                     dots:false
                 }}
+            />
+            <NewsList
+                type="card"
+                loadmore={true}
+                start={3}
+                amount={3}
             />
         </div>
     )
