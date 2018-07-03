14fac73f36a9d8e59571a50dfcc7007ed4edc735
lx kish <urban.ktulhu@gmail.com>
2018-07-03 23:20:39 +1200

    53 News List transitions

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -5,26 +5,27 @@ import axios from 'axios';
 
 import { URL } from '../../../config';
 import styles from './newsList.css';
+import Button from '../Buttons/button';
 
 class NewsList extends Component {
     state = {
-        items:[],
-        start:this.props.start,
-        end:this.props.start + this.props.amount,
-        amount:this.props.amount
+        items: [],
+        start: this.props.start,
+        end: this.props.start + this.props.amount,
+        amount: this.props.amount
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.request(this.state.start, this.state.end)
     }
 
     request = (start, end) => {
         axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
-        .then( response => {
-            this.setState({
-                items:[...this.state.items,...response.data]
+            .then(response => {
+                this.setState({
+                    items: [...this.state.items, ...response.data]
+                })
             })
-        })
     }
 
     loadMore = () => {
@@ -35,16 +36,27 @@ class NewsList extends Component {
     renderNews = (type) => {
         let template = null;
 
-        switch(type){
-            case('card'):
-                template = this.state.items.map((item,i) => (
-                    <div>
-                        <div className={styles.newslist_item}>
-                            <Link to={`/articles/${item.id}`}>
-                                <h2>{item.title}</h2>
-                            </Link>
+        switch (type) {
+            case ('card'):
+                template = this.state.items.map((item, i) => (
+                    <CSSTransition
+                        classNames={{
+                            enter:styles.newsList_wrapper,
+                            enterActive:styles.newsList_wrapper_enter
+                        }}
+                        timeout={500}
+                        key={i}
+                    >
+                        <div>
+                            <div className={styles.newslist_item}>
+                                <Link to={`/articles/${item.id}`}>
+                                        teams
+                                    <h2>{item.title}</h2>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
+                    </CSSTransition>
+
                 ))
                 break;
             default:
@@ -55,14 +67,20 @@ class NewsList extends Component {
 
     }
 
-    render(){
-        console.log(this.state.items)
-        return(
+    render() {
+        return (
             <div>
-                { this.renderNews( this.props.type ) }
-                <div onClick={() => this.loadMore()}>
-                    LOAD MORE
-                </div>
+                <TransitionGroup
+                    component="div"
+                    className="list"
+                >
+                    {this.renderNews(this.props.type)}
+                </TransitionGroup>
+                <Button
+                    type="loadmore"
+                    loadMore={() => this.loadMore()}
+                    cta="Load More News"
+                />
             </div>
         )
     }
