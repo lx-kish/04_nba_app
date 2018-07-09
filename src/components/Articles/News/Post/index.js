5c6e28388721a746aef15b3a96f17008b808678a
lx kish <urban.ktulhu@gmail.com>
2018-07-09 23:19:56 +1200

    60 Video post view

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -4,7 +4,6 @@ import { URL } from '../../../../config';
 
 import styles from '../../articles.css';
 import Header from './header';
-import Body from './body';
 
 class NewsArticles extends Component {
 
@@ -39,7 +38,17 @@ class NewsArticles extends Component {
                     date={article.date}
                     author={article.author}
                 />
-                <Body/>
+                <div className={styles.articleBody}>
+                    <h1>{article.title}</h1>
+                    <div className={styles.articleImage}
+                        style={{
+                            background:`url('/images/articles/${article.image}')`
+                        }}
+                    ></div>
+                    <div className={styles.articleText}>
+                        {article.body}
+                    </div>
+                </div>
             </div>
         )
     }
