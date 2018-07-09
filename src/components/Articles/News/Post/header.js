b49b94732eb433013b688c254028d155270bef40
lx kish <urban.ktulhu@gmail.com>
2018-07-09 13:22:39 +1200

    59 Article post view 2

--- src/components/Articles/News/Post/header.js
+++ src/components/Articles/News/Post/header.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import TeamNfo from '../../Elements/teamNfo';
+import PostData from '../../Elements/postData';
+
 const header = (props) => {
+
+    const teamNfo = (team) => {
+        return team ? (
+            <TeamNfo team={team}/>
+        ) : null;
+    }
+
+    const postData = (date, author) => (
+        <PostData data={{date, author}}/>
+    )
+
     return(
         <div>
-            header
+            {teamNfo(props.teamData)}
+            {postData(props.date, props.author)}
         </div>
     )
 }
